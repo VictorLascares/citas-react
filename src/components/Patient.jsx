1734f04cdf641dfc3f5abd48ffeccc7bbbac8d10
Victor Lascares <vmlascares@gmail.com>
2021-12-12 00:05:07 -0600

    Agregando el listado de pacientes y creando el componente Patient

---
+++ src/components/Patient.jsx
@@ -0,0 +1,24 @@
+
+const Patient = () => {
+    return (
+        <div className='m-3 bg-white shadow-md px-5 py-10 rounde-xl'>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: {''}
+                <span className='font-normal normal-case'>Doky</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Dueño: {''}
+                <span className='font-normal normal-case'>Victor</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Email: {''}
+                <span className='font-normal normal-case'>victor@correo.com</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha de registro: {''}
+                <span className='font-normal normal-case'>15 de Octubre del 2007</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas: {''}
+                <span className='font-normal normal-case'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus nostrum delectus fugiat tempora. Neque officiis similique, molestias enim sunt explicabo eos consectetur minus doloribus quod amet distinctio vero voluptatem provident.</span>
+            </p>
+        </div>
+    )
+}
+
+export default Patient
--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Patient from "./Patient"
 
 export const PatientList = () => {
     return (
-        <div className='w-1/2 lg:w-3/5'>
-            <h2>Pacientes</h2>
+        <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
+            <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
+            <p className='text-xl mt-5 mb-10 text-center'>
+                Aministra tus {''}
+                <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+            </p>
+
+            <Patient/>
+            <Patient/>
+            <Patient/>
+            <Patient/>
+            <Patient/>
         </div>
     )
 }
