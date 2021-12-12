9cedd4c9619c0c4986d64a6484216d334994a0a5
Victor Lascares <vmlascares@gmail.com>
2021-12-11 21:10:49 -0600

    Creando y llamando a los componentes Form y PatientList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./Header"
+import Form from "./Form"
+import PatientList from "./PatientList"
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Form/>
+      <PatientList/>
     </>
   )
 }
---
+++ src/components/PatientList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const PatientList = () => {
+    return (
+        <div>
+            <h2>Pacientes</h2>
+        </div>
+    )
+}
+
+export default PatientList
