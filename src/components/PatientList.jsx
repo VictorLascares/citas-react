88f418a6dc0b036c77e3d259c734b4d6485b5955
Victor Lascares <vmlascares@gmail.com>
2021-12-14 13:05:44 -0600

    Agregando textos de forma condicional

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -3,18 +3,30 @@ import Patient from "./Patient"
 export const PatientList = ({patients}) => {
     return (
         <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
-            <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
-            <p className='text-xl mt-5 mb-10 text-center'>
-                Aministra tus {''}
-                <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
-            </p>
-
-            {patients.map((element) => (
-                <Patient 
-                    key={element.id}
-                    patient={element}
-                />
-            ))}
+            {patients && patients.length ? (
+                <>
+                    <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
+                    <p className='text-xl mt-5 mb-10 text-center'>
+                        Aministra tus {''}
+                        <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+                    </p>
+        
+                    {patients.map((element) => (
+                        <Patient 
+                            key={element.id}
+                            patient={element}
+                        />
+                    ))}
+                </>
+            ) : (
+                <>
+                    <h2 className='font-black text-3xl text-center'>No hay Pacientes</h2>
+                    <p className='text-xl mt-5 mb-10 text-center'>
+                        Comienza agregando Pacientes {''}
+                        <span className='text-indigo-600 font-bold'> y aparecerán en este lugar</span>
+                    </p>
+                </>
+            )}
         </div>
     )
 }
